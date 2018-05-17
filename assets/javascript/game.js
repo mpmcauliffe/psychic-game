3deb0e94cd4271a39d3f852e643bc83ac9ed3777
Michael McAuliffe <mcauliffe99@gmail.com>
2018-05-17 00:21:18 -0400

    refactor game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 /* DOM objects and Array */
-let letters    = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+const letters  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     inputGuess = document.getElementById(`guess`),
     wins       = document.getElementById(`wins`),
@@ -9,7 +9,7 @@ let letters    = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
     taken      = document.getElementById(`taken`);
 
 /* global variables */
-let score, fail, left, tries, input, key;
+let score, fail, left, tries, choice, key;
 
 
 /* EVENTLISTENER:
@@ -21,65 +21,53 @@ window.addEventListener(`keyup`, () => {
     screenDOM.style.animation = `1.5s fadeout .5s forwards`;
     setTimeout(() => {    
         screenDOM.style.display = `none`;
-        mainController(); 
+        init(); 
     }, 1000);
 }, { once: true });
 
 
-/* USER DRIVEN & INTERNAL 
-        initializes and controls the game by calling the necessary functions
-        called from initial EVENTLISTENER */
-let mainController = () => {
-       
-    initializeBoard();
-    let inPlay = true;
+/* EVENTLISTENER
+        -gets keypress input from the user
+        -varifies the input
+        -compares the input to the key varible 
+        -updates DOM */
+window.addEventListener(`keypress`, () => {
+    let x = event.which || event.keyCode,
+        press = String.fromCharCode(x);
+    choice = press.toLocaleLowerCase();
+    
 
-    do {
-        key         = retrieveRandomLetter();
-        input       = makeGuess();
-        
-        inputGuess.innerHTML = input;
-
-        
-        inPlay = false;
-    } while (inPlay);
-        
-}
-
+});
 
 /* INTERNAL
-        resets variables and sets the innerHTML to reflect reset
-        called from mainController() */
-let initializeBoard = () => {
-    let score = 0,
-        fail = 0,
-        left = 10,
-        tries = 0,
-        input = "",
-        key = "";
+        using random number this grabs letter from letter[] and updates the value
+        of key
+        called from init() and keypress */
+const retrieveRandomLetter = () => {
+    let randomNum = Math.floor(Math.random() * 26),
+        key = letters[randomNum];
+}
+
+/* EVENTLISTENER:
+        resets game by calling the init() */
+document.getElementById(`reset`).addEventListener(`click`, () => {
+    init();
+})
+
+/* USER TRIGGER & INTERNAL 
+        this function resets the board to its initial state
+        called from initial EVENTLISTENER & BUTTON PRESS */
+const init = () => {
+    score   = 0,
+    fail    = 0,
+    left    = 10,
+    tries   = 0,
+    choice  = "",
+    
+    retrieveRandomLetter();
 
     wins.innerHTML = `wins: ${score}`;
     losses.innerHTML = `losses: ${fail}`;
     remaining.innerHTML = `remaining: ${left}`;
-    taken.innerHTML = `tries: ${tries}`;
-}
-
-
-/* INTERNAL
-        creates random number [0-25] as an index value for letters array
-        called from mainController()
-    RETURN
-         string of single letter from letters array */
-let retrieveRandomLetter = () => {
-    let randomNum   = Math.floor(Math.random() * 26),
-        singleLetter = letters[randomNum];
-    return singleLetter;
-}
-
-let makeGuess = () => {
-    window.addEventListener(`keypress`, () => {
-        let x       = event.which || event.keyCode,
-            press   = String.fromCharCode(x);
-        return press;
-    });
+    taken.innerHTML = `tries: ${tries}`;    
 }
