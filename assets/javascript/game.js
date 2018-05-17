3861cd42617b7d5078fe5ef864c593e67f09ef40
Michael McAuliffe <mcauliffe99@gmail.com>
2018-05-17 02:33:24 -0400

    logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,47 +6,110 @@ const letters  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
     wins       = document.getElementById(`wins`),
     losses     = document.getElementById(`losses`),
     remaining  = document.getElementById(`remaining`),
-    taken      = document.getElementById(`taken`);
+    taken      = document.getElementById(`taken`),
+    stat       = document.getElementById(`status`);
+
 
 /* global variables */
-let score, fail, left, tries, choice, key;
-
-
-/* EVENTLISTENER:
-        removes cover screen on keyup.
-        uses setTimeout 1s to remove cover screen to access reset button
-        calls mainController() */
-window.addEventListener(`keyup`, () => {
-    let screenDOM = document.querySelector(`.starter-cover`);
-    screenDOM.style.animation = `1.5s fadeout .5s forwards`;
-    setTimeout(() => {    
-        screenDOM.style.display = `none`;
-        init(); 
-    }, 1000);
-}, { once: true });
+let score, fail, left, tries, choice, hidden;
 
 
 /* EVENTLISTENER
+        document.getElementById(`guess`)
         -gets keypress input from the user
         -varifies the input
-        -compares the input to the key varible 
-        -updates DOM */
-window.addEventListener(`keypress`, () => {
-    let x = event.which || event.keyCode,
-        press = String.fromCharCode(x);
-    choice = press.toLocaleLowerCase();
+        -compares the input to the hidden varible 
+        -updates DOM 
+        -called from init() */
+let mainController = () => {
     
+    window.addEventListener(`keypress`, (event) => {
 
-});
+        // 1) extract input and make it usable
+        let x = event.which || event.key,
+            press = String.fromCharCode(x);
+            choice = press.toLocaleLowerCase();
+        console.log(choice);
+        // 2) letter varification
+        let isLetter = false; 
+        for(let i = 0; i < letters.length; i++) {
+            if(choice === letters[i]){
+                isLetter = true;
+            }
+        }
+        if(!isLetter) {
+            stat.textContent = `please enter a letter`;
+            stat.style.animation = `4s fadeout 2s forwards`;
+            mainController();
+        } else {
+
+            inputGuess.textContent = choice;
+
+            if(choice === hidden) {
+                stat.textContent = `You guessed it!`;
+                stat.style.animation = `4s fadeout 2s forwards`;
+
+                score++;
+                left = 10;
+                tries = 0;
+
+                setTimeout(() => {
+                    updateDOM();
+                    retrieveRandomLetter();
+                    mainController();
+                }, 3000);
+                              
+            } else {
+                stat.textContent = `You guessed wrong. Try again`;
+                stat.style.animation = `4s fadeout 2s forwards`;
+
+                left--;
+                tries++;
+
+                setTimeout(() => {
+                    updateDOM();
+                    mainController();
+                }, 3000);
+
+            }
+            if(left < 1) {
+                stat.textContent = `I was thinking of the letter ${hidden}. I'm thinking of another letter now.`;
+                stat.style.animation = `6s fadeout 2s forwards`;
+
+                fail++;
+                left = 10;
+                tries = 0;
+
+                setTimeout(() => {
+                    updateDOM();
+                    retrieveRandomLetter();
+                    mainController();
+                }, 3000);
+            } 
+        }
+    });
+}  
 
 /* INTERNAL
         using random number this grabs letter from letter[] and updates the value
-        of key
-        called from init() and keypress */
+        of hidden
+        called from event in mainController() and init() */
 const retrieveRandomLetter = () => {
     let randomNum = Math.floor(Math.random() * 26),
-        key = letters[randomNum];
+        hidden = letters[randomNum];
 }
+
+/* INTERNAL 
+        DOM updates in one place
+        called within the event of mainController() and init()*/
+const updateDOM = () => {
+    wins.innerHTML = `wins: ${score}`;
+    losses.innerHTML = `losses: ${fail}`;
+    remaining.innerHTML = `remaining: ${left}`;
+    taken.innerHTML = `tries: ${tries}`;
+    inputGuess.innerHTML = "_";
+}
+
 
 /* EVENTLISTENER:
         resets game by calling the init() */
@@ -54,20 +117,35 @@ document.getElementById(`reset`).addEventListener(`click`, () => {
     init();
 })
 
+
 /* USER TRIGGER & INTERNAL 
         this function resets the board to its initial state
         called from initial EVENTLISTENER & BUTTON PRESS */
 const init = () => {
-    score   = 0,
-    fail    = 0,
-    left    = 10,
-    tries   = 0,
-    choice  = "",
-    
-    retrieveRandomLetter();
+    score = 0,
+    fail = 0,
+    left = 10,
+    tries = 0,
+    choice = "",
 
-    wins.innerHTML = `wins: ${score}`;
-    losses.innerHTML = `losses: ${fail}`;
-    remaining.innerHTML = `remaining: ${left}`;
-    taken.innerHTML = `tries: ${tries}`;    
+    retrieveRandomLetter();
+    updateDOM();
+    mainController();
 }
+init();
+
+
+
+
+/* EVENTLISTENER:
+        removes cover screen on keyup.
+        uses setTimeout 1s to remove cover screen to access reset button
+        calls init() */
+// window.addEventListener(`keyup`, () => {
+//     let screenDOM = document.querySelector(`.starter-cover`);
+//     screenDOM.style.animation = `1.5s fadeout .5s forwards`;
+//     setTimeout(() => {    
+//         screenDOM.style.display = `none`;
+//         init(); 
+//     }, 1000);
+// }, { once: true });
